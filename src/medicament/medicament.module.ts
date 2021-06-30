a5755318a381ca4b9313f72c1229605ec10f8402
SiwarAmara <86376209+SiwarAmara@users.noreply.github.com>
2021-06-30 18:26:31 +0100

    repertoir2

--- src/medicament/medicament.module.ts
+++ src/medicament/medicament.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { medicamentEntity } from 'src/entities/medicament.entity';
+import { UserModule } from 'src/user/user.module';
 import { MedicamentController } from './medicament.controller';
 import { MedicamentService } from './medicament.service';
 
 @Module({
   controllers: [MedicamentController],
-  providers: [MedicamentService]
+  providers: [MedicamentService],
+  exports: [MedicamentService],
+  imports: [UserModule,TypeOrmModule.forFeature([medicamentEntity]), PassportModule.register({defaultStrategy: 'jwt'}),JwtModule.register({
+secret: 'nourSecretKey',
+signOptions: {
+  expiresIn: 3600
+}
+ })]
 })
 export class MedicamentModule {}

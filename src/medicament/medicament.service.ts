dbadc343ba6272362f960a459b9dac762b7df65b
SiwarAmara <86376209+SiwarAmara@users.noreply.github.com>
2021-06-30 18:16:24 +0100

    repertoire

--- src/medicament/medicament.service.ts
+++ src/medicament/medicament.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { medicamentEntity } from 'src/entities/medicament.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class MedicamentService {}
+export class MedicamentService {
+
+
+    constructor(
+        @InjectRepository(medicamentEntity)
+        private medicamentRepository: Repository<medicamentEntity>
+        
+      )
+       {}
+
+}
